9be7cb063c980963805a0697762d0d50e44b94bb
chenzhiy2001 <chenzhiy2001@qq.com>
2023-10-19 20:03:26 +0800

    添加真题套卷，一些自己做的习题

---
+++ slides/ppt说明.md
@@ -0,0 +1,3 @@
+os的过时了（太大了也传不到github上），直接看github上的即可 https://github.com/LearningOS/os-lectures
+
+ds的去邓公网站有
---
+++ 数学知识框架.mm
@@ -0,0 +1,126 @@
+<map version="freeplane 1.11.5">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="数学知识框架" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1697342116419">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<hook NAME="MapStyle" background="#002b36" zoom="1.948">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" associatedTemplateLocation="template:/dark_solarized_template.mm"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_1664261437" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#002b36" BACKGROUND_COLOR="#fdf6e3" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#fdf6e3" WIDTH="2" TRANSPARENCY="255" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1664261437" STARTINCLINATION="132.75 pt;-32.25 pt;" ENDINCLINATION="132.75 pt;24 pt;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#93a1a1" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#fdf6e3" BACKGROUND_COLOR="#073642">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#2aa198" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10" ITALIC="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#fdf6e3" BACKGROUND_COLOR="#d33682" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#d33682"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_1940777954" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_1940777954"/>
+<font NAME="Ubuntu" SIZE="12" BOLD="true"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#002b36" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt" BORDER_WIDTH="3.1 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#fdf6e3" BACKGROUND_COLOR="#073642" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#fdf6e3" BACKGROUND_COLOR="#586e75" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR="#f0f0f0">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#fdf6e3" BACKGROUND_COLOR="#657b83" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#fdf6e3" BACKGROUND_COLOR="#839496" BORDER_COLOR_LIKE_EDGE="true">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#fdf6e3" BACKGROUND_COLOR="#93a1a1" BORDER_COLOR_LIKE_EDGE="true">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#eee8d5" BORDER_COLOR_LIKE_EDGE="true">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#eedfcc" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#fdf6e3" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BORDER_COLOR="#f0f0f0">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="函数极限" POSITION="bottom_or_right" ID="ID_1200761953" CREATED="1697342173400" MODIFIED="1697342180091">
+<node TEXT="类型1" ID="ID_1807312000" CREATED="1697342329130" MODIFIED="1697342331340">
+<node TEXT="\latex \frac 0 0  \frac \infty \infty 0\cdot\infty 型" ID="ID_1182777401" CREATED="1697342340706" MODIFIED="1697342551444">
+<node TEXT="按顺序考虑最简单:(1)等价无穷小替换(2)泰勒公式(3)洛必达法则" ID="ID_1494523415" CREATED="1697342592558" MODIFIED="1697342633821"/>
+</node>
+</node>
+<node TEXT="类型2" ID="ID_855699623" CREATED="1697342331498" MODIFIED="1697342332960">
+<node TEXT="\latex \infty - \infty" ID="ID_1769373425" CREATED="1697342638576" MODIFIED="1697342660539">
+<node TEXT="通分或乘除某式化为类型1" ID="ID_1352000449" CREATED="1697342653556" MODIFIED="1697342692393"/>
+</node>
+</node>
+<node TEXT="类型3" ID="ID_683393497" CREATED="1697342333315" MODIFIED="1697342334629">
+<node TEXT="\latex 1^\infty" ID="ID_36002931" CREATED="1697342698898" MODIFIED="1697342709350">
+<node TEXT="\latex 运用 $\lim_{x\to0}\left(1+x\right)^{\frac{1}{x}}=e  $" ID="ID_1075492376" CREATED="1697342709935" MODIFIED="1697364636139"/>
+</node>
+<node TEXT="\latex 0^0 , \infty ^0" ID="ID_1703536129" CREATED="1697364644422" MODIFIED="1697373541364">
+<node TEXT="写成指数函数形式，化为类型1" ID="ID_1726974235" CREATED="1697373551628" MODIFIED="1697373573890"/>
+</node>
+</node>
+</node>
+<node TEXT="函数微专题" POSITION="bottom_or_right" ID="ID_1829290808" CREATED="1697342180668" MODIFIED="1697342196130">
+<node TEXT="函数性质" ID="ID_1947898593" CREATED="1697373580827" MODIFIED="1697373583828">
+<node TEXT="有界性、单调性、奇偶性、周期性" ID="ID_84563590" CREATED="1697373583922" MODIFIED="1697373614357"/>
+</node>
+<node TEXT="特殊函数" ID="ID_1745939431" CREATED="1697373618902" MODIFIED="1697373620713">
+<node TEXT="反函数、复合函数、分段函数" ID="ID_1232522034" CREATED="1697373621119" MODIFIED="1697373632796"/>
+</node>
+</node>
+<node TEXT="数列极限" POSITION="bottom_or_right" ID="ID_1045068449" CREATED="1697342196367" MODIFIED="1697342203391"/>
+<node TEXT="连续间断与导数" POSITION="bottom_or_right" ID="ID_439229117" CREATED="1697342203580" MODIFIED="1697342217077"/>
+<node TEXT="中值定理" POSITION="bottom_or_right" ID="ID_919973851" CREATED="1697342217263" MODIFIED="1697342232100"/>
+<node TEXT="导数应用" POSITION="bottom_or_right" ID="ID_1817746271" CREATED="1697342232296" MODIFIED="1697342239054"/>
+<node TEXT="导数证明" POSITION="bottom_or_right" ID="ID_1926776266" CREATED="1697342239223" MODIFIED="1697342245691"/>
+<node TEXT="积分1" POSITION="bottom_or_right" ID="ID_1378160473" CREATED="1697342245850" MODIFIED="1697342251759"/>
+<node TEXT="积分2" POSITION="bottom_or_right" ID="ID_639240151" CREATED="1697342251941" MODIFIED="1697342253015"/>
+<node TEXT="定积分应用" POSITION="bottom_or_right" ID="ID_1642684881" CREATED="1697342253202" MODIFIED="1697342259289"/>
+<node TEXT="多元微分概念" POSITION="bottom_or_right" ID="ID_649977795" CREATED="1697342259457" MODIFIED="1697342267472"/>
+<node TEXT="多元微分计算" POSITION="bottom_or_right" ID="ID_1286475158" CREATED="1697342267625" MODIFIED="1697342274442"/>
+<node TEXT="重积分" POSITION="bottom_or_right" ID="ID_1916359169" CREATED="1697342274595" MODIFIED="1697342279897"/>
+<node TEXT="微分方程" POSITION="bottom_or_right" ID="ID_1647163862" CREATED="1697342280055" MODIFIED="1697342286834"/>
+<node TEXT="曲线积分" POSITION="bottom_or_right" ID="ID_1454900038" CREATED="1697342287001" MODIFIED="1697342288764"/>
+<node TEXT="曲面积分" POSITION="bottom_or_right" ID="ID_1750710796" CREATED="1697342288896" MODIFIED="1697342298857"/>
+<node TEXT="级数判敛" POSITION="bottom_or_right" ID="ID_194717299" CREATED="1697342299021" MODIFIED="1697342310553"/>
+<node TEXT="幂级数" POSITION="bottom_or_right" ID="ID_49269620" CREATED="1697342310730" MODIFIED="1697342312109"/>
+</node>
+</map>
